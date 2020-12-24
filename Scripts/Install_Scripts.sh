c2037d9b675dfe8b968f673932d84880ffba6d16
exodious <exodious@users.noreply.github.com>
2020-12-23 20:07:10 -0500

    Remove world write

--- Scripts/Install_Scripts.sh
+++ Scripts/Install_Scripts.sh
@@ -2,7 +2,7 @@
 
 echo -e "\nCreating Refresh script in Moonlight..."
 
-if [ -d /home/pi/RetroPie/roms/moonlight ] 
+if [ -d /home/pi/RetroPie/roms/moonlight ]
 then
     rm -rf /home/pi/RetroPie/roms/moonlight
 fi
@@ -13,8 +13,6 @@ chmod a+x ./Scripts/Refresh.sh
 cp ./Scripts/Refresh.sh /home/pi/RetroPie/roms/moonlight/Refresh.sh
 cp ./GenerateGamesList.py /home/pi/RetroPie/roms/moonlight/GenerateGamesList.py
 cp ./Scripts/Force_Quit.sh /home/pi/RetroPie/roms/moonlight/Force_Quit.sh
-
-chmod 777 /home/pi/RetroPie/roms/moonlight
 
 echo -e "Refresh script has been added to RetroPie\n"
 echo -e "After loading RetroPie, use the \"Refresh\" rom listed in the Moonlight system.\n"
