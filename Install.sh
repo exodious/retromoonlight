9bc33721dce1a3e52c011249709a4ab7bafa857e
exodious <exodious@users.noreply.github.com>
2020-12-23 23:21:29 -0500

    Loop installer vs calling repeatedly

--- Install.sh
+++ Install.sh
@@ -2,55 +2,56 @@
 
 set -euo pipefail
 
-echo -e "\n****************************************************************"
-echo -e "Welcome to the Moonlight Installer Script for RetroPie"
-echo -e "****************************************************************\n"
-echo -e "Select an option:"
-echo -e " * 1: Complete Install and Setup"
-echo -e " * 2: Install Moonlight"
-echo -e " * 3: Pair Moonlight to PC"
-echo -e " * 4: Install Moonlight Menu in RetroPie"
-echo -e " * 5: Install Moonlight Refresh Script in RetroPie"
-echo -e " * 6: Install Moonlight themes"
-echo -e " * 7: Remove Launch Scripts"
-echo -e " * 8: Exit"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+SCRIPTS_DIR="${DIR}/Scripts"
 
-read NUM
-case $NUM in
-    1)
-        bash ./Scripts/Install_moonlight.sh
-        bash ./Scripts/Pair_moonlight.sh
-        bash ./Scripts/Install_moonlight_menu.sh
-        bash ./Scripts/Install_Scripts.sh
-        bash ./Scripts/Install_themes.sh
-        ;;
-    2)
-        bash ./Scripts/Install_moonlight.sh
-        bash ./Install.sh
-        ;;
-    3)
-        bash ./Scripts/Pair_moonlight.sh
-        bash ./Install.sh
-        ;;
-    4)
-        bash ./Scripts/Install_moonlight_menu.sh
-        bash ./Install.sh
-        ;;
-    5)
-        bash ./Scripts/Install_Scripts.sh
-        bash ./Install.sh
-        ;;
-    6)
-        bash ./Scripts/Install_themes.sh
-        bash ./Install.sh
-        ;;
-    7)
-        echo -e "\nRemoving all Moonlight launch scripts..."
-        rm -rf /home/pi/RetroPie/roms/moonlight
-        bash ./Install.sh
-        ;;
-    8)
-        exit 1
-        ;;
-    *) echo "INVALID NUMBER!" ;;
-esac
+while true; do
+    echo -e "\n****************************************************************"
+    echo -e "Welcome to the Moonlight Installer Script for RetroPie"
+    echo -e "****************************************************************\n"
+    echo -e "Select an option:"
+    echo -e " * 1: Complete Install and Setup"
+    echo -e " * 2: Install Moonlight"
+    echo -e " * 3: Pair Moonlight to PC"
+    echo -e " * 4: Install Moonlight Menu in RetroPie"
+    echo -e " * 5: Install Moonlight Refresh Script in RetroPie"
+    echo -e " * 6: Install Moonlight themes"
+    echo -e " * 7: Remove Launch Scripts"
+    echo -e " * 8: Exit"
+
+    read NUM
+    case $NUM in
+        1)
+            ${SCRIPTS_DIR}/Install_moonlight.sh
+            ${SCRIPTS_DIR}/Pair_moonlight.sh
+            ${SCRIPTS_DIR}/Install_moonlight_menu.sh
+            ${SCRIPTS_DIR}/Install_Scripts.sh
+            ${SCRIPTS_DIR}/Install_themes.sh
+            ;;
+        2)
+            ${SCRIPTS_DIR}/Install_moonlight.sh
+            ;;
+        3)
+            ${SCRIPTS_DIR}/Pair_moonlight.sh
+            ;;
+        4)
+            ${SCRIPTS_DIR}/Install_moonlight_menu.sh
+            ;;
+        5)
+            ${SCRIPTS_DIR}/Install_Scripts.sh
+            ;;
+        6)
+            ${SCRIPTS_DIR}/Install_themes.sh
+            ;;
+        7)
+            echo -e "\nRemoving all Moonlight launch scripts..."
+            rm -rf "~pi/RetroPie/roms/moonlight"
+            ;;
+        8)
+            exit 0
+            ;;
+        *)
+            echo "INVALID NUMBER!"
+            ;;
+    esac
+done
