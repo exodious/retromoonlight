9ef472ba9c25928e69f4a93bcf50b0b6b1b2267b
exodious <exodious@users.noreply.github.com>
2020-12-24 01:28:48 -0500

    Fix path

--- Scripts/Install_Scripts.sh
+++ Scripts/Install_Scripts.sh
@@ -12,9 +12,9 @@ fi
 
 mkdir -p "$MOONLIGHT_ROMS_DIR"
 
-install ${DIR}/Scripts/Refresh.sh ${MOONLIGHT_ROMS_DIR}/
-install ${DIR}/GenerateGamesList.py ${MOONLIGHT_ROMS_DIR}/
-install ${DIR}/Scripts/Force_Quit.sh ${MOONLIGHT_ROMS_DIR}/
+install ${DIR}/Refresh.sh ${MOONLIGHT_ROMS_DIR}/
+install ${DIR}/../GenerateGamesList.py ${MOONLIGHT_ROMS_DIR}/
+install ${DIR}/Force_Quit.sh ${MOONLIGHT_ROMS_DIR}/
 
 echo -e "Refresh script has been added to RetroPie\n"
 echo -e "After loading RetroPie, use the \"Refresh\" rom listed in the Moonlight system.\n"
