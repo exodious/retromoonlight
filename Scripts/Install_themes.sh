528a3571a4b6fd228bc2f2b6b175c12e4e4cd7b1
exodious <exodious@users.noreply.github.com>
2021-01-05 00:40:07 -0500

    Fix themes path

--- Scripts/Install_themes.sh
+++ Scripts/Install_themes.sh
@@ -5,6 +5,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 echo -e "\nInstalling Moonlight compatible themes..."
 
-sudo cp -R ${DIR}/themes /etc/emulationstation/
+sudo cp -R ${DIR}/../themes /etc/emulationstation/
 
 echo -e "\nThemes installed"
